4946ddafc24973ed6253bae9bd74ce573a0f1ea6
Anh <hna09011997@gmail.com>
2021-05-12 15:49:03 +0700

    test git

--- Halloween Sale.c
+++ Halloween Sale.c
@@ -16,19 +16,7 @@ char** split_string(char*);
 
 int parse_int(char*);
 
-/*
- * Complete the 'howManyGames' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER p
- *  2. INTEGER d
- *  3. INTEGER m
- *  4. INTEGER s
- */
-
-int howManyGames(int p, int d, int m, int s) {
-    // Return the number of games you can buy
+int howManyGames(int p, int d, int m, int s) {    
     int i = 0;
     while (s >= 0) {
         int k = p - (d*i);
